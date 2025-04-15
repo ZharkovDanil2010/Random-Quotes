ab4f071ff1e86d529910ec6f71cc0825fad17275
oleg1523 <danik.kabanik2010@gmail.com>
2025-04-15 22:49:05 +0300

    now we can add quotes to favorities below

---
+++ quotes.js
@@ -0,0 +1,24 @@
+const quotes = [
+  { quote: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt", isFavorite: false, },
+  { quote: "In the middle of every difficulty lies opportunity.", author: "Albert Einstein", isFavorite: false, },
+  { quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.", author: "Winston Churchill", isFavorite: false, },
+  { quote: "Don't watch the clock; do what it does. Keep going.", author: "Sam Levenson", isFavorite: false, },
+  { quote: "The best way to predict the future is to invent it.", author: "Alan Kay", isFavorite: false, },
+  { quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt", isFavorite: false, },
+  { quote: "Everything you have ever wanted is on the other side of fear.", author: "George Addair", isFavorite: false, },
+  { quote: "Act as if what you do makes a difference. It does.", author: "William James", isFavorite: false, },
+  { quote: "Hardships often prepare ordinary people for an extraordinary destiny.", author: "C.S. Lewis", isFavorite: false, },
+  { quote: "If you want to achieve greatness stop asking for permission.", author: "Anonymous", isFavorite: false, },
+  { quote: "Do one thing every day that scares you.", author: "Eleanor Roosevelt", isFavorite: false, },
+  { quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.", author: "Zig Ziglar", isFavorite: false, },
+  { quote: "Opportunities don't happen. You create them.", author: "Chris Grosser", isFavorite: false, },
+  { quote: "The harder you work for something, the greater you'll feel when you achieve it.", author: "Unknown", isFavorite: false, },
+  { quote: "Push yourself, because no one else is going to do it for you.", author: "Unknown", isFavorite: false, },
+  { quote: "Dream it. Wish it. Do it.", author: "Unknown", isFavorite: false, },
+  { quote: "Success does not just find you. You have to go out and get it.", author: "Unknown", isFavorite: false, },
+  { quote: "Great things never come from comfort zones.", author: "Unknown", isFavorite: false, },
+  { quote: "Don't stop when you're tired. Stop when you're done.", author: "Marilyn Monroe", isFavorite: false, },
+  { quote: "Wake up with determination. Go to bed with satisfaction.", author: "Unknown", isFavorite: false, },
+];
+
+export default quotes
--- rn.js
+++ rn.js
@@ -1,18 +1,50 @@
-const quotes = [
-  { text: "Будь собой — прочие роли уже заняты.", author: "Оскар Уайльд" },
-  { text: "Успех — это движение от неудачи к неудаче без потери энтузиазма.", author: "Уинстон Черчилль" },
-  { text: "Логика приведет вас от А к Б. Воображение — куда угодно.", author: "Альберт Эйнштейн" },
-  { text: "Жизнь — это 10% того, что с тобой происходит, и 90% как ты на это реагируешь.", author: "Чарльз Р. Суинделл" }
-];
+import quotes from "./quotes.js";
 
 const btn = document.querySelector('.quote-btn');
 const quoteText = document.querySelector('.quote-text');
 const quoteAuthor = document.querySelector('.quote-author');
+const toggleFavoriteBtn = document.querySelector('.favorite-btn')
+const favoriteContainer = document.querySelector('.favorities_container')
+
+let currentIndex 
 
 btn.addEventListener('click', () => {
-  const random = quotes[Math.floor(Math.random() * quotes.length)];
-  quoteText.textContent = random.text;
-  quoteAuthor.textContent = random.author;
+  currentIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentIndex];
+  quoteText.textContent = randomQuote.quote;
+  quoteAuthor.textContent = randomQuote.author;
+  const currentQuote = quotes[currentIndex];
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite 
+    ? 'Remove from favorities'
+    : 'Add to favorities'
+
+    toggleFavoriteBtn.style.display = 'inline-block';
 });
 
+
+toggleFavoriteBtn.addEventListener('click', () => {
+  const currentQuote = quotes[currentIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite 
+    ? 'Remove from favorities'
+    : 'Add to favorities'
+
+    if (currentQuote.isFavorite) {
+      const favoriteCard = document.createElement('div');
+      favoriteCard.classList.add('favorite-card')
+      favoriteCard.innerHTML = `
+        <p class="favorite-quote">${currentQuote.quote}</p>
+        <p class="favorite-author">${currentQuote.author}</p>
+      `;
+      favoriteContainer.appendChild(favoriteCard);
+    } else {
+      const favoriteCards = favoriteContainer.querySelectorAll('.favorite-card');
+      favoriteCards.forEach(card => {
+       if (card.textContent.includes(currentQuote.quote)) {
+          card.remove();
+        }
+      });
+    }
+  }
+)
 
